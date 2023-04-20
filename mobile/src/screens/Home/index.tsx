7fc133eaae84bec2fb7c003c26879a3e514b833c
Nicolas Moises <nicolas.nicc21@gmail.com>
2023-04-20 17:40:55 -0300

    Fix some bug on views list, and add the Axios lib to the communication with api

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,56 +1,76 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { Image, FlatList } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 
-import { GameCard, GameCardProps } from '../../components/GameCard';
-import { Heading } from '../../components/Heading';
-import { Background } from '../../components/Background';
-import { useNavigation } from '@react-navigation/native';
+import { GameCard, GameCardProps } from "../../components/GameCard";
+import { Background } from "../../components/Background";
+import { Heading } from "../../components/Heading";
+
 import { styles } from './styles';
+import { api } from "../../lib/api";
+import { Loading } from "../../components/Loading";
 
 export function Home() {
-    const [games, setGames] = useState<GameCardProps[]>([]);
+  const [games, setGames] = useState<GameCardProps[]>([])
+  const [isFetching, setIsFetching] = useState(true)
 
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
-        navigation.navigate('game', { id, title, bannerUrl });
-    }
+  function handleOpenGaming({ id, title, bannerUrl }: GameCardProps) {
+    navigation.navigate('game', { id, title, bannerUrl });
+  }
 
+  async function fetchGames() {
+   try {
+    const response = await api.get('/games')
 
-    useEffect(() => {
-        fetch('http://192.168.1.9:3333/games')
-            .then(response => response.json())
-            .then(data => setGames(data));
-    }, [])
+    setGames(response.data)
+   } catch(err) {
+    console.log(err)
+   } finally {
+    setIsFetching(false)
+   }
+  }
+
+  useEffect(() => {
+    fetchGames()
+  }, []);
+
+  if(isFetching) {
     return (
-        <Background>
-            <SafeAreaView style={styles.container}>
-                <Image
-                    source={logoImg}
-                    style={styles.logo}
-                />
-                <Heading
-                    title="Encontre seu duo!"
-                    subtitle="Selecione o game que deseja jogar..."
-                />
+        <Loading />
+    )
+  }
+  return (
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image
+          source={logoImg}
+          style={styles.logo}
+        />
 
-                <FlatList
-                    data={games}
-                    keyExtractor={item => item.id}
-                    renderItem={({ item }) => (
-                        <GameCard
-                            data={item}
-                            onPress={() => handleOpenGame(item)} />
-                    )}
-                    showsHorizontalScrollIndicator={false}
-                    horizontal
-                    contentContainerStyle={styles.contentList}
-                />
+        <Heading
+          title="Encontre seu duo!"
+          subtitle="Selecione o game que deseja jogar..."
+        />
 
-            </SafeAreaView>
-        </Background>
-    );
+        <FlatList
+          data={games}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <GameCard
+              data={item}
+              onPress={() => handleOpenGaming(item)}
+            />
+          )}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          contentContainerStyle={styles.contentList}
+        />
+      </SafeAreaView>
+    </Background>
+  );
 }
